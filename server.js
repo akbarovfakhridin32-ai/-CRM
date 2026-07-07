45ae4be64da57308583cec75ed894003c83d3ae4
akbarovfakhridin32-ai <akbarovfakhridin32@gmail.com>
2026-07-07 17:25:02 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const PORT = 3000;
 
 app.use(express.static(__dirname));
 
+// Маршрут для healthcheck
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Добавляем задержку 5 секунд перед запуском сервера
-setTimeout(() => {
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log('✅ Сервер запущен на порту ' + PORT);
-    });
-}, 5000);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('✅ Сервер запущен на порту ' + PORT);
+});
